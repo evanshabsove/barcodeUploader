69664953fd7c133e5b7724e84ec93ac6b798fbf9
Evan Shabsove <evan.shabsove@gmail.com>
2017-02-01 10:21:49 -0500

    python file exports png file barcode tried to make it function but did not work. Moving on to csv file

--- app/main.py
+++ app/main.py
@@ -4,3 +4,11 @@ from barcode.writer import ImageWriter
 ean = barcode.get('ean13', '123456789102', writer=ImageWriter())
 filename = ean.save('ean13')
 filename
+
+# def makingBarcode(name, barcode):
+#     from barcode.writer import ImageWriter
+#     ean = barcode.get(name, barcode, writer=ImageWriter())
+#     filename = ean.save(name)
+#     filename
+#
+# makingBarcode("test", '123456789102')
