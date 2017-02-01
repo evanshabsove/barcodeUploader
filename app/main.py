e3c94cc9d384902c84b2b90270ec5d4b6101324f
Evan Shabsove <evan.shabsove@gmail.com>
2017-02-01 14:06:22 -0500

    changed to a function, looking at user interfaces now

--- app/main.py
+++ app/main.py
@@ -3,26 +3,27 @@ from barcode.writer import ImageWriter
 from PIL import Image
 import csv
 
+def badgeGenerator(confirmation):
 
-confirmation = "XRV29M"
-
-with open('Databased.csv', 'rt') as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-        for field in row:
-            if field == confirmation:
-                foundBarcode = row[11]
-                name = row[1] + ' ' + row[2]
+    with open('Databased.csv', 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            for field in row:
+                if field == confirmation:
+                    foundBarcode = row[11]
+                    name = row[1] + ' ' + row[2]
 
 
-CODE = barcode.get_barcode_class('code128')
-code = CODE(foundBarcode, writer=ImageWriter())
-fullname = code.save(name)
+    CODE = barcode.get_barcode_class('code128')
+    code = CODE(foundBarcode, writer=ImageWriter())
+    fullname = code.save(name)
 
-badge = Image.open('badge2017.png', 'r')
-img = Image.open(name + ".png", 'r')
-img_w, img_h = img.size
-badge_w, badge_h = badge.size
-offset = ((badge_w - img_w)/2, (badge_h - img_h)/2 + 30)
-badge.paste(img, offset)
-badge.save('out.png')
+    badge = Image.open('badge2017.png', 'r')
+    img = Image.open(name + ".png", 'r')
+    img_w, img_h = img.size
+    badge_w, badge_h = badge.size
+    offset = ((badge_w - img_w)/2, (badge_h - img_h)/2 + 30)
+    badge.paste(img, offset)
+    badge.save('out.png')
+
+badgeGenerator('9VBZAA')
