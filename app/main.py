769b48fcaf5bb91aa41ea2f1555d2f0a71ff7519
Evan Shabsove <evan.shabsove@gmail.com>
2017-02-07 16:54:14 -0500

    added a bit of colour so it dosn't look terrible, as well as increased font size so things look bigger

--- app/main.py
+++ app/main.py
@@ -20,9 +20,7 @@ image = 'BADGE DESIGN.png'
 csv_path = 'Databased.csv'
 # main function that prints off logo
 def badgeGenerator(*args):
-    confirmation = confirmation_code_entry.get("1.0",END)
-    confirmation = str(confirmation)
-    confirmation = confirmation.rstrip()
+    confirmation = confirmation_code.get()
     foundBarcode = 1
     with open(csv_path, 'rt') as f:
         reader = csv.reader(f, delimiter=',')
@@ -36,6 +34,7 @@ def badgeGenerator(*args):
 
     if foundBarcode == 1:
         tkMessageBox.showerror("Error", "Did not match any codes, try again (CASE SENSATIVE)")
+        confirmation_code_entry.delete(0, END)
     else:
         CODE = barcode.get_barcode_class('code128')
         code = CODE(foundBarcode, writer=ImageWriter())
@@ -55,22 +54,25 @@ def badgeGenerator(*args):
         draw.text((1165, 1825),name,(0,0,0),font=font)
         draw.text((365, 1825),name,(0,0,0),font=font)
         img.save('outtext.jpg')
-
+        confirmation_code_entry.delete(0, END)
 
 # tkinter program
 root = Tk()
 root.title("Print your badge")
 
-mainframe = ttk.Frame(root, padding="100 100 100 100")
+gui_style = ttk.Style()
+gui_style.configure('My.TButton', foreground='#334353')
+gui_style.configure('My.TFrame', background='#334353')
+
+mainframe = ttk.Frame(root, padding="500 300 500 300", style='My.TFrame')
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
 confirmation_code = StringVar()
-
-confirmation_code_entry = Text(mainframe, width=20)
+confirmation_code_entry = ttk.Entry(mainframe, font = "Helvetica 44", justify='center', width=12, textvariable=confirmation_code)
 confirmation_code_entry.grid(column=2, row=1, sticky=(W, E))
-b = ttk.Button(mainframe, text="Submit", command=badgeGenerator)
+b = Button(mainframe, text="Submit", font = "Helvetica 38", width=10, command=badgeGenerator)
 b.grid(column = 1, row = 1)
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
